0bd1fbe8380fb660069026d8f61550ce048e14c9
viswanath reddy <viswanath3344@gmail.com>
2018-05-09 14:49:58 +0530

    Expand TextView

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TextViewExapansion' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'GrowingTextView', '~> 0.4'
+
+  # Pods for TextViewExapansion
+
+end
--- TextViewExapansion/ViewController.swift
+++ TextViewExapansion/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import GrowingTextView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,GrowingTextViewDelegate  {
 
+    
+    @IBOutlet weak var messageTextView: GrowingTextView!
+    
+    
+    @IBOutlet weak var threeLabel: UILabel!
+    @IBOutlet weak var fourLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        messageTextView.delegate  = self
+        
+       // messageTextView.translatesAutoresizingMaskIntoConstraints = true
+       // messageTextView.isScrollEnabled = false
+       // messageTextView.sizeToFit()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func textViewDidChangeHeight(_ textView: GrowingTextView, height: CGFloat) {
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+       
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+       
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+  
 }
-
